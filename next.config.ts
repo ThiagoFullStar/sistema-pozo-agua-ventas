048dfd9162ade8cf44818a2055018bbb746210c1
ThiagoFullStar <thiagoamoresb@gmail.com>
2025-08-04 16:26:14 -0500

    Add next.config.ts 1754342777528

---
+++ next.config.ts
@@ -0,0 +1,21 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        pathname: '/photos/**',
+      },
+    ],
+  },
+  assetPrefix: '',
+  basePath: '',
+  distDir: 'out'
+}
+
+export default nextConfig
